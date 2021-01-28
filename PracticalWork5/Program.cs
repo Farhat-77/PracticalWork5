8f36403e0fa74d83ba99b7ba119c79a5a23618e3
Bola_sw25@student.itstep.kz <Bola_sw25@student.itstep.kz>
2021-01-28 19:08:20 +0600

    Добавьте файлы проекта.

---
+++ PracticalWork5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PracticalWork5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find your Account Sid and Token at twilio.com/console
+            // and set the environment variables. See http://twil.io/secure
+            string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+            string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "This is the ship that made the Kessel Run in fourteen parsecs?",
+                from: new Twilio.Types.PhoneNumber("+15017122661"),
+                to: new Twilio.Types.PhoneNumber("+15558675310")
+            );
+
+            Console.WriteLine(message.Sid);
+        }
+    }
+}
